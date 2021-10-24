0d421fed6fd703bd9ca278cbe36ddce2af331c10
toast <toast@big-toaster>
2021-10-24 17:46:52 -0400

    Initial seed

---
+++ init.vim
@@ -0,0 +1,40 @@
+call plug#begin('~/.config/nvim/plugged')
+Plug 'nvim-lua/plenary.nvim' " General
+Plug 'nvim-lua/popup.nvim' " General
+Plug 'ThePrimeagen/harpoon'
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'nvim-telescope/telescope-fzy-native.nvim'
+Plug 'frenchtoasters/telescope-kubectl.nvim'
+Plug 'frenchtoasters/telescope-linode.nvim'
+Plug 'AckslD/nvim-neoclip.lua'
+Plug 'vim-airline/vim-airline' " Bottom bar fancy stuff
+Plug 'vim-airline/vim-airline-themes' " Bottom bar fancy stuff
+Plug 'junegunn/seoul256.vim' " Theme
+Plug 'lukas-reineke/indent-blankline.nvim'
+Plug 'mhinz/vim-signify' " Git changes on side file :cheifskiss:
+Plug 'heavenshell/vim-pydocstring', { 'do': 'make install' }
+Plug 'antoinemadec/FixCursorHold.nvim'
+Plug 'APZelos/blamer.nvim' " Git blamer
+Plug 'terrortylor/nvim-comment' " Comment hot keys
+"Plug 'neoclide/coc.nvim', {'branch': 'release'} " coc-go coc-pairs coc-lua coc-python coc-snippets
+Plug 'neovim/nvim-lspconfig'
+Plug 'williamboman/nvim-lsp-installer'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'L3MON4D3/LuaSnip'
+Plug 'saadparwaiz1/cmp_luasnip'
+Plug 'windwp/nvim-autopairs'
+Plug 'tzachar/cmp-tabnine', { 'do': './install.sh' }
+Plug 'fatih/vim-go', { 'do': ':GoUpdateBinaries' } " gofmt, goimports
+call plug#end()
+" Personal config
+autocmd VimEnter * lua require('settings')
+autocmd VimEnter * lua require('keymap')
+autocmd VimEnter * lua require('toast')
+autocmd VimEnter * LspStart
+color seoul256
+hi NonText guifg=250 guifg=none
+hi Normal guifg=252 guibg=none
+hi DiffDelete guifg=#ff5555 guibg=none
---
+++ lua/toast.lua
@@ -0,0 +1,132 @@
+local actions = require('telescope.actions')
+local cmp = require('cmp')
+local lspconfig = require('lspconfig')
+local servers = {"gopls", "pyright", "terraformls", "dockerls", "bashls", "ansiblels", "vimls"}
+
+USER = vim.fn.expand('$USER')
+
+local sumneko_root_path = ""
+local sumneko_binary = ""
+
+if vim.fn.has("mac") == 1 then
+    sumneko_root_path = "/Users/" .. USER .. "/.config/nvim/lua-language-server"
+    sumneko_binary = "/Users/" .. USER .. "/.config/nvim/lua-language-server/bin/macOS/lua-language-server"
+elseif vim.fn.has("unix") == 1 then
+    sumneko_root_path = "/home/" .. USER .. "/.config/nvim/lua-language-server"
+    sumneko_binary = "/home/" .. USER .. "/.config/nvim/lua-language-server/bin/Linux/lua-language-server"
+else
+    print("Unsupported system for sumneko")
+end
+
+require("telescope").setup({
+	defaults= {
+		file_sorter = require("telescope.sorters").get_fzy_sorter,
+		prompt_prefix = " > ",
+		color_devicons = true,
+		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+		mappings = {
+			i = {
+				["<esc>"] = actions.close,
+			}
+		}
+	},
+	pickers = {
+		find_files = {
+			theme = "ivy",
+		},
+		live_grep = {
+			theme = "ivy",
+		},
+		buffers = {
+			theme = "ivy",
+		},
+		help_tags = {
+			theme = "ivy",
+		},
+	},
+	extensions = {
+		k8s_commands = {
+			kubeconfig = "/home/toast/.kube/test-cofig"
+		},
+	}
+})
+
+require('nvim_comment').setup({
+	comment_empty = false,
+	line_mapping = "<leader>cl",
+	operator_mapping = "<leader>c"
+})
+
+require('nvim-treesitter.configs').setup({
+	highlight = {
+		enable = true
+	},
+	textobjects = {
+		enable = true
+	},
+	ensure_installed = "maintained"
+})
+
+require('nvim-autopairs').setup()
+require('harpoon').setup()
+require('neoclip').setup({
+	default_register = {'"', '+', '*'}
+})
+require("telescope").load_extension("fzy_native")
+require("telescope").load_extension("k8s_commands")
+require("telescope").load_extension("linode_commands")
+require("telescope").load_extension("neoclip")
+require("telescope").load_extension("docker_commands")
+
+require("indent_blankline").setup({
+	char = "┊",
+	show_current_context = true,
+})
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+	mapping = {
+		['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    	['<C-f>'] = cmp.mapping.scroll_docs(4),
+    	['<C-Space>'] = cmp.mapping.complete(),
+    	['<C-e>'] = cmp.mapping.close(),
+    	['<CR>'] = cmp.mapping.confirm({ select = true }),
+	},
+	sources = {
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+		{ name = 'buffer' },
+	},
+})
+
+for _, lsp in ipairs(servers) do
+	lspconfig[lsp].setup({
+		capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	})
+end
+
+require('lspconfig').sumneko_lua.setup {
+    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
+    settings = {
+        Lua = {
+            runtime = {
+                version = 'LuaJIT',
+                path = vim.split(package.path, ';')
+            },
+            diagnostics = {
+                globals = {'vim'}
+            },
+            workspace = {
+                library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true}
+            }
+        }
+    },
+	capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+}
+
